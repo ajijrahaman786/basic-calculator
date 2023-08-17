d0f0b7522f8ba39c7101d193e5ed5d3e0dadca49
Ajij Rahaman <ajij@appycodes.com>
2023-08-17 18:27:07 +0530

    calculator

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 // get all btn
-let clickBtn    = document.querySelectorAll('.btn-grey');
+const clickBtn    = document.querySelectorAll('.btn-grey');
 console.log(clickBtn);
-let display     = document.querySelector(".display");
-let btnOperator = document.querySelectorAll('.btn-operator');
-// set click event for each of btn
-clickBtn.forEach(function(btn, index){
-    btn.addEventListener("click", function(){
+const display     = document.querySelector(".display");
+const btnOperator = document.querySelectorAll('.btn-operator');
+const equal_btn		= document.querySelector('.btn-equal');
+// set click event for number btn
+clickBtn.forEach(function(btnNum, index){
+    btnNum.addEventListener("click", function(){
         
         let btnVal = document.getElementById(`btn${index+1}`).value;
         console.log(btnVal);
@@ -15,4 +16,21 @@ clickBtn.forEach(function(btn, index){
     });
 
 
+});
+
+// set click event for operator btn
+
+btnOperator.forEach(function(btnOp, index){
+    btnOp.addEventListener("click", function(){
+        let operatorVal = document.getElementById(`operator${index+1}`).value;
+        console.log(operatorVal);
+        display.textContent += operatorVal;
+
+    });
+
+
+});
+
+equal_btn.addEventListener("click", function() {
+	display.textContent = eval(display.textContent);
 });
